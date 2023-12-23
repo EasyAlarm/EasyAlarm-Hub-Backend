8a9226b0bb524ec3f1a3b21e58b474ec528799c4
gNash-dev <gNash-dev@protonmail.com>
2023-12-23 22:45:11 +0000

    remove hub settings and use base http response in hub controller

--- src/controllers/hubController.ts
+++ src/controllers/hubController.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import IHubSettings from '../hub/IHubSettings';
+import { IAlarmSettings } from '../hub/types/interfaces/IHubSettings';
 import { ArmHubInput } from '../schemas/hubSchema';
-import { UpdateHubSettingsSchema } from '../schemas/hubSettingsSchema';
-import { armHub, disarmHub, getHubSettings, getHubStatus, panicHub, updateHubSettings } from '../services/hubService';
+import { armHub, disarmHub, getHubStatus, panicHub } from '../services/hubService';
 import { doesProfileExist } from '../services/profileService';
 import ApiError from '../utils/apiError';
+import { BaseHttpResponse } from '../utils/baseHttpResponse';
 import catchAsync from "../utils/catchAsync";
 
 export const armHubHandler = catchAsync(async (req: Request<ArmHubInput['params']>, res: Response, next: NextFunction) =>
@@ -14,50 +14,32 @@ export const armHubHandler = catchAsync(async (req: Request<ArmHubInput['params'
         return next(new ApiError("Profile does not exist", 400));
     }
 
-
     armHub(req.params.profileName);
 
-    res.status(200).send("Hub armed");
+    const response = BaseHttpResponse.successMessageResponse("Hub armed");
+    res.json(response);
 });
 
 export const disarmHubHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) =>
 {
     disarmHub();
-    res.status(200).send("Hub disarmed");
+
+    const response = BaseHttpResponse.successMessageResponse("Hub disarmed");
+    res.json(response);
 });
 
 export const panicHubHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) =>
 {
     panicHub();
-    res.status(200).send("Hub panicked");
+
+    const response = BaseHttpResponse.successMessageResponse("Hub panicked");
+    res.json(response);
 });
 
 export const getHubStatusHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) =>
 {
     const status = await getHubStatus();
-    res.status(200).send({ data: status });
-});
 
-export const getHubSettingsHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) =>
-{
-    const settings = await getHubSettings();
-    res.status(200).send({ data: settings });
-});
-
-export const updateHubSettingsHandler = catchAsync(async (req: Request<{}, {}, UpdateHubSettingsSchema['body']>, res: Response, next: NextFunction) =>
-{
-    const hubSettings: IHubSettings =
-    {
-        //convert to int 
-        armDelay: parseInt(req.body.armDelay),
-        alarmDuration: parseInt(req.body.alarmDuration),
-        alarmDelay: parseInt(req.body.alarmDelay),
-        alarmOnOfflineUnit: false
-    };
-    
-    console.log(hubSettings);
-
-    await updateHubSettings(hubSettings);
-
-    res.status(200).send("Hub settings updated");
+    const response = BaseHttpResponse.successResponse(status);
+    res.json(response);
 });
